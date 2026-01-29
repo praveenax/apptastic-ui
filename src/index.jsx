f89c2f88f018668901d7bc5791d415756adb98d9
praveen kumar a x <xav.axp@gmail.com>
2026-01-29 12:12:37 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,4 @@
 export { SocialButtons } from "./components/SocialButtons";
 export { ReadModeToggle } from "./components/ReadModeToggle";
+export { WhatsappProgress } from "./components/WhatsappProgress";
+export { ArticleCarousel } from "./components/ArticleCarousel";
