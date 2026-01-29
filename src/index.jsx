7f05343e30f2639b961647c4a93522439cce62e9
praveen kumar a x <xav.axp@gmail.com>
2026-01-29 16:03:29 +0530

    Added reference to util functions

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,8 @@
+// Components
 export { SocialButtons } from "./components/SocialButtons";
 export { ReadModeToggle } from "./components/ReadModeToggle";
 export { WhatsappProgress } from "./components/WhatsappProgress";
 export { ArticleCarousel } from "./components/ArticleCarousel";
+
+// Utils
+export * from "./utils";
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export { splitAndNormalizeSections } from "./splitAndNormalizeSections";
